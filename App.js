bb96b458298f72b87eb38fa1cb6bc5e99fffc29f
Esnor Noel Enrique Vaca Moreno <enevaca@pj-scr.poderjudicial.gov.bo>
2018-02-06 13:48:58 -0400

    Contenedores con Scroll

--- App.js
+++ App.js
@@ -11,8 +11,10 @@ import {
   Text,
   View,
   Image,
+  ScrollView,
 } from 'react-native';
 import { Icon } from 'react-native-elements';
+import ArtistBox from './ArtistBox';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -25,30 +27,21 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
 
-    const image = 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png'
-    const name = 'David Bowie'
-    const likes = 200
-    const comments = 140
-
+    const artist = {
+      image: 'https://lastfm-img2.akamaized.net/i/u/300x300/31a51f6e3ec647c8997150ec837891c7.png',
+      name: 'David Brownie',
+      likes: 2000,
+      comments: 140,
+    }
+    
     return (
-      <View style={styles.container}>
-        <View style={styles.artistBox} >
-          <Image style={styles.image} source={{ uri: image }} />
-          <View style={styles.info}>
-            <Text style={styles.name}>{name}</Text>
-            <View style={styles.row}>
-              <View style={styles.iconContainer}>
-                <Icon name='ios-heart-outline' type='ionicon' size={30} color="gray" />
-                <Text style={styles.count}>{likes}</Text>
-              </View>
-              <View style={styles.iconContainer}>
-                <Icon name='ios-chatboxes-outline' type='ionicon' size={30} color="gray" />
-                <Text style={styles.count}>{comments}</Text>
-              </View>
-            </View>
-          </View>
-        </View>
-      </View>
+      <ScrollView style={styles.container}>
+        {
+          Array(500).fill(artist).map(artist => {
+            return <ArtistBox artist={artist} />
+          })
+        }
+      </ScrollView>
     );
   }
 }
@@ -63,32 +56,4 @@ const styles = StyleSheet.create({
     width: 150,
     height: 150,
   },
-  artistBox: {
-    backgroundColor: 'white',
-    flexDirection: 'row',
-  },
-  info: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  name: {
-    fontSize: 20,
-    marginTop: 10,
-    color: '#333',
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginHorizontal: 40,
-    marginTop: 15,
-  },
-  iconContainer: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  count: {
-    color: 'gray',
-  }
 });
---
+++ ArtistBox.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+import { Icon } from 'react-native-elements';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class ArtistBox extends Component {
+  render() {
+    //console.warn(this.props.artist.name)
+    const { image, name, likes, comments} = this.props.artist
+
+    return (
+      <View style={styles.artistBox} >
+        <Image style={styles.image} source={{ uri: image }} />
+        <View style={styles.info}>
+          <Text style={styles.name}>{name}</Text>
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Icon name='ios-heart-outline' type='ionicon' size={30} color="gray" />
+              <Text style={styles.count}>{likes}</Text>
+            </View>
+            <View style={styles.iconContainer}>
+              <Icon name='ios-chatboxes-outline' type='ionicon' size={30} color="gray" />
+              <Text style={styles.count}>{comments}</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  artistBox: {
+    margin: 5,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    shadowColor: 'black',
+    shadowOpacity: .2,
+    shadowOffset: {
+      height: 1,
+      width: -2,
+    },
+    elevation: 2,
+  },
+   image: {
+    width: 150,
+    height: 150,
+  },
+  info: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  name: {
+    fontSize: 20,
+    marginTop: 10,
+    color: '#333',
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 40,
+    marginTop: 15,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  count: {
+    color: 'gray',
+  }
+});
